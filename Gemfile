fc6409fcefe5b655113f87c846afe2576dca4e0c
Aleksey Ivanov <mov.stox@gmail.com>
2014-10-07 19:28:08 +0300

    Added letter_opener_web to see outgoing emails

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'dotenv-rails'
+  gem 'letter_opener_web', '~> 1.2.0'
 end
 
 #Report all errors
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
