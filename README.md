1aa5b345e299cb8825586d9bc3b784cceb8b93aa
Aleksey Ivanov <mov.stox@gmail.com>
2014-10-07 19:35:42 +0300

    Setup Mandrill for production

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 #Environment vars
 
-* APP_NAME - default app name (i.e. using it in db config)
-* ERRBIT_APP_API_KEY - app api key
-* ERRBIT_HOST - Errbit server hostname
+* Generic:
+
+ * APP_NAME - default app name (i.e. using it in db config)
+ * ERRBIT_APP_API_KEY - app api key
+ * ERRBIT_HOST - Errbit server hostname
+
+* Production only:
+ * Setup vars on Heroku with `heroku config:set VAR=VALUE`
+ * Mandrill setup with `heroku addons:add mandrill`
+   * MANDRILL_PRODUCTION_USERNAME - Mandrill username for SMTP
+   * MANDRILL_PRODUCTION_APIKEY - Mandrill api key for SMTP
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+      :address   => 'smtp.mandrillapp.com',
+      :port      => 587,
+      :user_name => ENV['MANDRILL_PRODUCTION_USERNAME'],
+      :password  => ENV['MANDRILL_PRODUCTION_APIKEY']
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { host: ENV['APP_NAME']+'.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: 'utf-8'
 end
